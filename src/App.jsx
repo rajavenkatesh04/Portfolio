ebe80f3b0d8f7b7fe2cf2dd351cc5063184c7e95
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-01-25 06:25:00 +0530

    Add speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, useLocation } from "react-router-dom";
-import { scroller } from "react-scroll";
+// import { scroller } from "react-scroll";
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
 import { Analytics } from '@vercel/analytics/react';
+
 
 function App() {
 
