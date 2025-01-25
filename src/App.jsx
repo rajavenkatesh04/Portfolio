02caf0c7921bc15ccc1d71a235f61f6333f98bcc
rajavenkatesh04 <rg3941@srmist.edu.in>
2025-01-25 08:05:55 +0530

    Add speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Hero from './components/Hero';
 import Skills from './components/Skills';
 // import Projects from "./components/Projects";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
       {/* <Projects /> */}
       <Footer />
       <Analytics />
+      <SpeedInsights />
     </Router>
   );
 }
